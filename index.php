f82f3cb1e7d84efa8b3a4a02258f38e37e7f74ab
Schutze4K <natalialara960@gmail.com>
2025-09-24 16:39:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
     <title>Hola</title>
 </head>
 <body>
-<h1>Fecha: <?= date(date"d.m.y")?></h1>
-<?php
-    if (date ("d") % 2 == 0) {
-        echo "Hoy es día par";
-    } else {
-        echo "Hoy es un día impar";
-    }
-?>
+    <h1>Fecha: <?= date("d.m.y") ?></h1>
+    <?php
+        if (date("d") % 2 == 0) {
+            echo "Hoy es día par";
+        } else {
+            echo "Hoy es un día impar";
+        }
+    ?>
 </body>
 </html>
